bf01231ce128fe28b44cbd0d21d06e0f79e5f622
Lucas <lucasjosafe@gmail.com>
2026-08-26 20:20:16 -0300

    Ahora PublicacionServicio.js agrega modalidad y duración

--- gestor-de-publicaciones/PublicacionServicio.js
+++ gestor-de-publicaciones/PublicacionServicio.js
@@ -1,7 +1,16 @@
 import { Publicacion } from "./Publicacion.js";
 
 export class PublicacionServicio extends Publicacion {
-  constructor(titulo, descripcion, autor) {
+  constructor(titulo, descripcion, autor, modalidad, duracion) {
     super(titulo, descripcion, autor);
+    this.modalidad = modalidad;
+    this.duracion = duracion;
+  }
+
+  mostrarResumen() {
+    return (
+      super.mostrarResumen() +
+      ` - Modalidad: ${this.modalidad} - Duración: ${this.duracion}`
+    );
   }
 }
