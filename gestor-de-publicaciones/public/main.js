c4aa80ef1ec7d8b5682daaa711b8ff4d236b3e77
Lucas <lucasjosafe@gmail.com>
2026-08-31 21:24:36 -0300

    Se actualizan los campos en tiempo real dependiendo si es venta o servicio.

--- gestor-de-publicaciones/public/main.js
+++ gestor-de-publicaciones/public/main.js
@@ -238,3 +238,23 @@ function actualizarVistaPrevia() {
 titulo.addEventListener("input", actualizarVistaPrevia);
 autor.addEventListener("input", actualizarVistaPrevia);
 tipo.addEventListener("change", actualizarVistaPrevia);
+
+// --- Parte 4: Change adapta el formulario ---
+const camposEspecificos = document.getElementById("campos-especificos");
+
+function actualizarCamposEspecificos() {
+  if (tipo.value === "venta") {
+    camposEspecificos.innerHTML = `
+    <input id="precio" type="number" placeholder="Precio">
+    <input id="stock" type="number" value="1">`;
+  } else {
+    camposEspecificos.innerHTML = `
+    <select id="modalidad">
+      <option>presencial</option><option>virtual</option>
+    </select>
+    <input id="duracion" type="number" placeholder="Minutos">`;
+  }
+}
+tipo.addEventListener("change", actualizarCamposEspecificos);
+actualizarCamposEspecificos();
+
